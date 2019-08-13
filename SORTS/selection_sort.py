e7982ff6df9bb3ce0deed842902ff3bf356dbbe1
Curry <chenxiaojun@01zhuanche.com>
2019-08-13 17:21:44 +0800

    add selection sort implement in python

---
+++ SORTS/selection_sort.py
@@ -0,0 +1,31 @@
+"""
+
+Implement selection sort in python
+
+Input an array a
+
+Algorithm:
+    Iterate i from 0 to N-1 (len(a) - 1):
+        1.find the index min of smallest remaining (j from j+1 to N-1) entry
+        2.Swap a[i] and a[min]
+
+Output sorted a
+
+"""
+
+def selection_sort(a=[]):
+    N = len(a)
+    for i in range(N):
+        min = i
+        for j in range(i+1, N):
+            if a[j] < a[min]:
+                min = j
+
+        swap = a[min]
+        a[min] = a[i]
+        a[i] = swap
+    return a
+
+if __name__ == "__main__":
+    array = [5,4,3,2,1]
+    print(selection_sort(array))
